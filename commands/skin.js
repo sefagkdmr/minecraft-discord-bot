873bd2c822a1b024bb0cc9558b3011ce42f77c75
sefagkdmr <52085784+sefagkdmr@users.noreply.github.com>
2022-04-30 23:40:38 +0300

    Add files via upload

---
+++ commands/skin.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = global.client;
+const config = require("../config.js")
+module.exports = {
+	name: 'skin',
+	description: 'skin',
+	async execute(message, args) {
+        let mesaj = args.slice(1).join(' ')
+        let member = message.mentions.members.first()
+        if (!args[0]) return message.channel.send(`:x: Bir seçenek belirtmelisin. Seçenekler \`avatar\`, \`kafa\`, \`vücut\` veya \`skin\` şeklinde olmalıdır. \`${config.bot.prefix}skin kafa Sefa_gkdmr\``)
+        if (!mesaj) return message.channel.send(`:x: Bir kullanıcı adı belirtmelisin. \`${config.bot.prefix}skin kafa Sefa_gkdmr\``);
+        if (mesaj == member) return message.channel.send(`:x: Bu bir kullanıcı adı değil, bir kullanıcı adı belirtmelisin.`)
+
+        if (args[0] == 'avatar') {
+            const webAttachment = new Discord.MessageAttachment(`https://mc-heads.net/avatar/${mesaj}.png`, `${mesaj}.png`)
+            message.channel.send({files: [webAttachment]})
+        }
+
+        if (args[0] == 'kafa') {
+            const webAttachment = new Discord.MessageAttachment(`https://mc-heads.net/head/${mesaj}.png`, `${mesaj}.png`)
+            message.channel.send({files: [webAttachment]})
+        }
+
+        if (args[0] == 'vücut') {
+            const webAttachment = new Discord.MessageAttachment(`https://mc-heads.net/body/${mesaj}.png`, `${mesaj}.png`)
+            message.channel.send({files: [webAttachment]})
+        }
+
+        if (args[0] == 'skin') {
+            const webAttachment = new Discord.MessageAttachment(`https://mc-heads.net/skin/${mesaj}.png`, `${mesaj}.png`)
+            message.channel.send({files: [webAttachment]})
+        }
+    },
+}
